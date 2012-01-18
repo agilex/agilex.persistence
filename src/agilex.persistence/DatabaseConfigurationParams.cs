6908c1e429cae6b7c147471ae766a608a02366f0
Nic Pillinger <nic@agilex.co.uk>
2012-01-18 10:22:34 +0000

    add default DatabaseConfigurationParams class

---
+++ src/agilex.persistence/DatabaseConfigurationParams.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace agilex.persistence
+{
+    public class DatabaseConfigurationParams : IDatabaseConfigurationParams
+    {
+        public DatabaseConfigurationParams(string appSettingKeyForDbConnectionString, IEnumerable<Assembly> assemblies,
+                                           string schemaExportLocation, bool blowDbAway)
+        {
+            BlowDbAway = blowDbAway;
+            SchemaExportLocation = schemaExportLocation;
+            Assemblies = assemblies;
+            AppSettingKeyForDbConnectionString = appSettingKeyForDbConnectionString;
+        }
+
+        #region IDatabaseConfigurationParams Members
+
+        public string AppSettingKeyForDbConnectionString { get; private set; }
+        public IEnumerable<Assembly> Assemblies { get; private set; }
+        public string SchemaExportLocation { get; private set; }
+        public bool BlowDbAway { get; private set; }
+
+        #endregion
+    }
+}
